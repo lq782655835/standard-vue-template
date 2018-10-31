d0082fa3878cd6cbe114ab504e4b0ec8de64b67f
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-10-31 17:11:01 +0800

    choice add light-weight ui

--- meta.js
+++ meta.js
@@ -19,7 +19,16 @@ module.exports = {
       author: {
         type: "string",
         message: "Author"
+      },
+      ui: {
+        type: "confirm",
+        message: "Set up light-weight components?"
       }
+    },
+    filters: {
+      'src/components/common/**/*': 'ui',
+      'src/components/u-demo-item.vue': 'ui',
+      'src/pages/demo.vue': 'ui'
     },
     completeMessage:
       "{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}"
